ea771fa9c22a2a55e89dd1c0f3c12d22ac68fa6a
nishamvp <nishamvp30@gmail.com>
2023-02-22 11:50:19 +0530

    horror movies added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import NavBar from './Components/NavBar/NavBar';
 import './App.css'
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
-import { action, comedy, originals } from './urls';
+import { action, comedy, originals,horror } from './urls';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <RowPost url={originals} title='Netflix Originals' />
       <RowPost url={action} title='Action' isSmall/>
       <RowPost url={comedy} title='Comedy' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
     </div>
   );
 }
